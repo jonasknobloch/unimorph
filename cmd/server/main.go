c6ba607765d17ff490e6efde4f1e1ea730f3fc8f
Jonas Knobloch <jonas.knobloch@t-online.de>
2023-12-19 18:23:00 +0100

    Print results

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jonasknobloch/unimorph"
 	"log"
 	"os"
@@ -9,18 +10,28 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting server...")
+
 	u := &unimorph.Unimorph{}
+
+	fmt.Println("Initializing unimorph dictionary", "$HOME/.unimorph/ces/ces")
 
 	if err := u.Init(filepath.Join(os.Getenv("HOME"), ".unimorph/ces/ces")); err != nil {
 		log.Fatalln(err)
 	}
 
+	fmt.Println("Listing on socket", "/tmp/unimorph.sock")
+
 	err := unimorph.Server("/tmp/unimorph.sock", func(form string) []byte {
 		split, ok := u.Split(form, unimorph.SplitSiblings(2))
 
 		if !ok {
+			fmt.Printf("%v\t%s\n", ok, form)
+
 			return []byte(form)
 		}
+
+		fmt.Printf("%v\t%s\n", ok, strings.Join(split, "#"))
 
 		return []byte(strings.Join(split, "#"))
 	})
